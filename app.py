7da5dae3c0b48065e144db6b4e3ac98f5da63505
Braulio Pérez <braulioperez.contacto@gmail.com>
2023-12-04 16:43:04 -0600

    comentarios hechos en archivos python

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np
 import os
 from modelo import LimpiezaDatos, Entrenamiento, Prediccion
 
-
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'csv'}
@@ -30,26 +29,25 @@ def upload_file():
         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filename)
 
-        # Process the uploaded CSV file (e.g., read into a DataFrame)
-        # You can perform additional processing or analysis on 'df' if needed.
+        # Procesar el archivo CSV cargado (por ejemplo, leerlo en un DataFrame)
+        # Puedes realizar procesamiento o análisis adicional en 'df' si es necesario.
 
         return render_template('index.html', message='File uploaded successfully')
 
     return render_template('index.html', message='Invalid file type')
 
-
 @app.route('/clean', methods=["POST", "GET"])
 def clean_file():
-    # Cargamos la dirección del archivo
+    # Cargar la dirección del archivo
     filename = os.path.join(app.config['UPLOAD_FOLDER'], 'housing.csv')
     
-    # cargamos los valores de q1 y q3 para la limpieza
+    # Cargar los valores de q1 y q3 para la limpieza desde el formulario HTML
     q1_value = float(request.form["q1_value"]) * 0.01
     q3_value = float(request.form["q3_value"]) * 0.01
 
-    # Asignamos el housing.csv a nuestro dataframe como df y nuestra clase
+    # Asignar el archivo housing.csv a nuestro DataFrame como df y nuestra clase
     df = LimpiezaDatos(datos=filename, q1=float(q1_value), q3=float(q3_value))
-    # Aplicamos la eliminación de nulos
+    # Aplicar la eliminación de nulos
     df = df.Super_limpieza()
     filename = os.path.join(app.config['UPLOAD_FOLDER'], 'dataset_limpio.csv')
 
@@ -59,21 +57,20 @@ model = None
 
 @app.route('/train', methods=["POST", "GET"])
 def train_model():
-    # Seleccionamos el archivo de datos limpio
+    # Seleccionar el archivo de datos limpios
     current_directory = os.getcwd()
     file_path = os.path.join(current_directory, "dataset_muy_limpio.csv")
 
-    # Extraemos tamaño del test desde html
+    # Extraer el tamaño del conjunto de prueba desde el formulario HTML
     test_size = float(request.form["test_size"]) * 0.01
 
-    # Se asigna el modelo a una variable y se le pasa el método de entrenar
-    modelo = Entrenamiento(datos = file_path, test_size= test_size)
+    # Asignar el modelo a una variable y pasarle el método de entrenamiento
+    modelo = Entrenamiento(datos=file_path, test_size=test_size)
     modelo_final = modelo.entrenar()
     global model
     model = modelo_final
 
     return redirect(url_for('index'))
-
 
 prediccion_lista = None
 model = model
@@ -82,6 +79,7 @@ model = model
 def predict():
     global model
     global prediccion_lista
+    # Obtener valores del formulario HTML para realizar la predicción
     ocean_distance = float(request.form["ocean_distance"])
     latitude = float(request.form["latitude"])
     longitude = float(request.form["longitude"])
@@ -92,16 +90,11 @@ def predict():
     households = float(request.form["households"])
     median_income = float(request.form["median_income"])
 
-    #bedroom_ratio,household_rooms
-
-    resultado = Prediccion(modelo=model, lon=longitude,lat=latitude,h_m_a=housing_median_age,t_rooms=total_rooms, t_bed=total_bedrooms, pop=population,house=households, m_i=median_income, ocean_distance=ocean_distance)    
-    valor_predicho =  resultado.Predecir()
+    # Realizar la predicción utilizando el modelo entrenado
+    resultado = Prediccion(modelo=model, lon=longitude, lat=latitude, h_m_a=housing_median_age, t_rooms=total_rooms, t_bed=total_bedrooms, pop=population, house=households, m_i=median_income, ocean_distance=ocean_distance)
+    valor_predicho = resultado.Predecir()
 
     return render_template("index.html", prediction_message=f"La casa vale ${valor_predicho} pesos")
-
-
-
-
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
--- modelo.py
+++ modelo.py
@@ -8,10 +8,12 @@ from sklearn.metrics import mean_squared_error, r2_score
 class ProcesoDatos():
 
     def __init__(self, datos_limpieza, datos_entrenamiento, test_size):
+        # Inicializar las instancias de LimpiezaDatos y Entrenamiento
         self.limpieza = LimpiezaDatos(datos_limpieza)
         self.entrenamiento = Entrenamiento(datos_entrenamiento, test_size)
 
     def ejecutar_proceso(self):
+        # Realizar la limpieza, normalización y entrenamiento de datos
         datos_limpios = self.limpieza.nulos_normalizacion()
         datos_sin_outliers = datos_limpios.outliers_delete()
         modelo_entrenado = self.entrenamiento.entrenar(datos_sin_outliers)
@@ -20,50 +22,40 @@ class ProcesoDatos():
 class LimpiezaDatos():
 
     def __init__(self, datos, q1, q3):
+        # Inicializar con el archivo de datos, q1 y q3 para la limpieza
         self.q1 = q1
         self.q3 = q3
         self.datos = pd.read_csv(datos)
 
     def Super_limpieza(self):
-
-        #Eliminación de nulos
+        # Eliminación de nulos
         self.datos.dropna(inplace=True)
 
-        # Al haber analizado el dataset previamente, ahora sabemos que hay que transformar al columna median_income a un float
+        # Transformación de la columna median_income a float
         self.datos["median_income"] = self.datos["median_income"].apply(lambda x: x.replace(" ", ""))
         self.datos["median_income"] = self.datos["median_income"].astype("float")
 
-        # Se pasa un logaritmo de las siguientes columnas para obtener una distribucion normal de ellas (se le suma 1 para evitar valores de 0 y no afecta proporciones)
+        # Aplicar logaritmo a ciertas columnas para obtener una distribución normal
         self.datos["total_rooms"] = np.log(self.datos["total_rooms"] + 1)
         self.datos["total_bedrooms"] = np.log(self.datos["total_bedrooms"] + 1)
         self.datos["population"] = np.log(self.datos["population"] + 1)
         self.datos["households"] = np.log(self.datos["households"] + 1)
 
-        # Se hace una codificación ordinal en la columna ocean proximity y se dropea ISLAND y las filas que tuvieran ese valor
+        # Codificación ordinal en la columna ocean proximity y eliminación de ISLAND
         self.datos = self.datos.join(pd.get_dummies(self.datos.ocean_proximity, dtype=int)).drop(["ocean_proximity"], axis=1)
         self.datos = self.datos[self.datos["ISLAND"] != 1]
         self.datos = self.datos.drop(["ISLAND"], axis=1)
         self.datos["bedroom_ratio"] = self.datos["total_bedrooms"] / self.datos["total_rooms"]
         self.datos["household_rooms"] = self.datos["total_rooms"] / self.datos["households"]
 
-        
-        # Proceso de eliminación de outliers
+        # Eliminación de outliers
         q1 = self.datos.quantile(self.q1)
         q3 = self.datos.quantile(self.q3)
-
         IQR = q3 - q1 
-
-        # Definir los límites inferior y superior para identificar outliers
         lower_bound = q1 - 1.5 * IQR
         upper_bound = q3 + 1.5 * IQR
-
-        # Filtrar filas que están dentro de los límites
         outliers = (self.datos < lower_bound) | (self.datos > upper_bound)
-
-        # Contar el número de outliers en cada fila
         outliers_count = outliers.sum(axis=1)
-
-        # Eliminar filas que contienen al menos un outlier
         self.datos = self.datos[outliers_count == 0]
 
         # Guardar el DataFrame limpio en un archivo CSV
@@ -72,57 +64,43 @@ class LimpiezaDatos():
         # Devolver el DataFrame limpio
         return self.datos
 
-
 class Entrenamiento():
 
     def __init__(self, datos, test_size):
+        # Inicializar con el archivo de datos y el tamaño del conjunto de prueba
         self.datos = pd.read_csv(datos)
         self.test_size = test_size
 
     def entrenar(self):
-        # Seleccionar el 5% para excluirlo
+        # Excluir el 5% de los datos para evaluar el rendimiento
         five_percent = self.datos.sample(frac=0.05, random_state=42)
         self.datos = self.datos.drop(five_percent.index)
 
-        # Seleccionar las columnas importantes para la prediccion
-        # Ajustar la lista segun los criterios
-        # Seleccionamos todas las columnas excepto "median_house_value"
+        # Seleccionar columnas importantes para la predicción
         features = self.datos.drop(["median_house_value"], axis=1)
-        # La columna 'median_house_value' es nuestra variable de objetivo
         target = self.datos["median_house_value"]
 
-        # Iniciamos la division para hacer nuestro entrenamiento
+        # Dividir el conjunto de datos en entrenamiento y prueba
         X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=self.test_size, random_state=42)
 
-        # Normalizamos las caracteristicas para asegurar que todos tengan la misma escala
+        # Normalizar las características
         scaler = StandardScaler()
         X_train_scaled = scaler.fit_transform(X_train)
         X_test_scaled = scaler.transform(X_test)
 
-        # Creamos el modelo de regresio lineal
+        # Crear y entrenar el modelo de regresión lineal
         model = LinearRegression()
-
-        # Entrenamos al modelo con los datos de entrenamiento
         model = model.fit(X_train_scaled, y_train)
-        y_pred = model.predict(X_test_scaled)
 
-        #Evaluamos el rendimiento del modelo
-        mse = mean_squared_error(y_test, y_pred)
-
-        # Buscamos el R2 de nuestro dataframe
-        r2 = r2_score(y_test, y_pred)
         return model
-
-
-
 
 class Prediccion():
 
     def __init__(self, lon, lat, h_m_a, t_rooms, t_bed, pop, house, m_i, modelo, ocean_distance):
-        bed_ra=t_bed/t_rooms
-        h_rooms=t_rooms/house
-
-        data = [lon,lat,h_m_a, np.log(t_rooms+1),np.log(t_bed+1),np.log(pop+1),np.log(house+1),m_i]
+        # Crear un array con datos de usuario para predecir
+        bed_ra = t_bed / t_rooms
+        h_rooms = t_rooms / house
+        data = [lon, lat, h_m_a, np.log(t_rooms + 1), np.log(t_bed + 1), np.log(pop + 1), np.log(house + 1), m_i]
 
         if ocean_distance == 1:
             data.extend([0, 1, 0, 0, bed_ra, h_rooms])
@@ -133,12 +111,10 @@ class Prediccion():
         else:
             data.extend([0, 0, 0, 1, bed_ra, h_rooms])
 
-
         self.user_data = np.array(data).reshape(1, -1)
         self.modelo = modelo
         
     def Predecir(self):
+        # Realizar la predicción con el modelo entrenado
         resultado = self.modelo.predict(self.user_data)
         return resultado[0]
-    
-
